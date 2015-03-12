c94f4f70ba323782d30ad79afe89fb77a416e2d8
mattgd <matt.dzwonczyk@live.com>
2015-03-12 12:31:56 -0400

    Began working on user input support.

--- calc.js
+++ calc.js
@@ -1,5 +1,6 @@
 var todayDate = new Date(); // Should be A day
 var finalText;
+var dayName = "";
 
 if (isSummer(todayDate)) {
     finalText = "It's your summer break, don't worry about it!";
@@ -39,13 +40,51 @@ if (isSummer(todayDate)) {
         verb = "was";
     }
 
-    finalText = "Today is " + compileFullDate(todayDate) + " and it " + verb + " a" + day;
+    finalText = "<div class='date'>" + compileFullDate(todayDate) + "</div>, and it " + verb + " a" + day;
 }
 
 /* Set the 'day' div to display the date and day */
 $(document).ready(function() {
-    $(".day").text(finalText);
+    $(".day").html(finalText);
+    $(".date").click(function() {
+        if ($("input").attr("id") != "userDate") {
+            $(".date").html("<input id='userDate' type='text' maxlength='10' onkeyup='addSlashes(this);' placeholder='MM/DD/YYYY'>");
+        } else {
+            $("#userDate").keyup(function() {
+
+                // Get the limit from maxlength attribute
+                var limit = parseInt($(this).attr('maxlength'));
+                //get the current text inside the textarea
+                var text = $(this).val();
+
+                if (parseInt(text.substr(0, 2)) > 12) {
+                    text = 12 + text.substr(2);
+                }
+
+                //count the number of characters in the text
+                var chars = text.length;
+
+                //check if there are more characters then allowed
+                if(chars > limit){
+                    //and if there are use substr to get the text before the limit
+                    var newText = text.substr(0, limit);
+
+                    //and change the current text with the new text
+                    $(this).val(newText);
+                }
+            });
+        }
+    });
 });
+
+function addSlashes(input) {
+    var v = input.value;
+    if (v.match(/^\d{2}$/) !== null) {
+        input.value = v + '/';
+    } else if (v.match(/^\d{2}\/\d{2}$/) !== null) {
+        input.value = v + '/';
+    }
+}
 
 /* Returns either A or B day */
 function getDay(date) {
@@ -56,10 +95,8 @@ function getDay(date) {
         // Skip weekends
         if (masterDate.getDay() != 0 && masterDate.getDay() != 6 && !isDayOff(masterDate)) {
             if (day == 'A') {
-                console.log("B");
                 day = 'B';
             } else {
-                console.log("A");
                 day = 'A';
             }
         }
@@ -81,7 +118,6 @@ function isSummer(date) {
 
 /* Check if today is a day off. Returns boolean value. */
 function isDayOff(date) {
-
     // Check if 'date' is a weekend
     if (date.getDay() == 0 || date.getDay() == 6) {
         return true;
@@ -100,36 +136,38 @@ function isDayOff(date) {
     return false;
 }
 
-// Make a non-abbreviated string for the date
-function compileFullDate(date) {
-    var fullDate;
-
-    switch(todayDate.getDay()) {
+function getDayName(date) {
+    switch(date.getDay()) {
         case 0:
-            fullDate = "Sunday, ";
+            return "Sunday, ";
             break;
         case 1:
-            fullDate = "Monday, ";
+            return "Monday, ";
             break;
         case 2:
-            fullDate = "Tuesday, ";
+            return "Tuesday, ";
             break;
         case 3:
-            fullDate = "Wednesday, ";
+            return "Wednesday, ";
             break;
         case 4:
-            fullDate = "Thursday, ";
+            return "Thursday, ";
             break;
         case 5:
-            fullDate = "Friday, ";
+            return "Friday, ";
             break;
         case 6:
-            fullDate = "Saturday, ";
+            return "Saturday, ";
             break;
     }
+}
+
+// Make a non-abbreviated string for the date
+function compileFullDate(date) {
+    var fullDate = "Today is " + getDayName(date);
 
     // Get the month
-    switch(todayDate.getMonth()) {
+    switch(date.getMonth()) {
         case 0:
             fullDate += "January ";
             break;
@@ -168,6 +206,6 @@ function compileFullDate(date) {
             break;
     }
 
-    fullDate += todayDate.getDate() + ", " + todayDate.getFullYear() + ", ";
+    fullDate += date.getDate() + ", " + date.getFullYear();
     return fullDate;
 }
